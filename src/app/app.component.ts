356009246fccfbd2e56a65f038f34f3d92b001aa
theodorejohn7 <theodorejohn.s@10decoders.in>
2023-01-31 00:21:53 +0530

    sample binding data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  public message: string = 'Welcome to Angular Training';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
